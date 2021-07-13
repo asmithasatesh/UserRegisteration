984a2d6badb5ffd20b176d22eed0f013e40bc21f
asmithasatesh <asmithasatesh@gmail.com>
2021-07-13 17:17:06 +0530

    [Asmitha]Initial commit with Welcome message

---
+++ UserRegisteration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegisteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registeration Program!");
+        }
+    }
+}
