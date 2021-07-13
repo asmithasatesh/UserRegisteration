4f25942bae8418839af577f85d8a25930cbab71c
asmithasatesh <asmithasatesh@gmail.com>
2021-07-13 17:45:56 +0530

    [Asmitha]Get valid LastName from user

--- UserRegisteration/UserDetails.cs
+++ UserRegisteration/UserDetails.cs
@@ -7,11 +7,14 @@ namespace UserRegisteration
 {
     class UserDetails
     {
+        public static string firstname;
+        public static string lastname;
         public static void GetUserInformation()
         {
             Regex regex = new Regex(@"^[A-Z][a-z]{2,}$");
-            string firstname;
-            Console.WriteLine("\nEnter First Name starting with Caps(min 3 characters)");
+
+            //Get Last Name from user
+            Console.WriteLine("\nEnter First Name starting with Caps(minimum 3 characters)");
             while (true)
             {
                 firstname=Console.ReadLine();
@@ -24,7 +27,24 @@ namespace UserRegisteration
                     Console.WriteLine("Please enter a Valid First Name!");
                 }
             }
+
+            //Get Last Name from user
+            Console.WriteLine("\nEnter Last Name starting with Caps(minimum 3 characters)");
+            while (true)
+            {
+                lastname = Console.ReadLine();
+                if (regex.IsMatch(lastname))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a Valid Name Name!");
+                }
+            }
             Console.WriteLine("Your First name is: {0}", firstname);
+            Console.WriteLine("Your Last name is: {0}", lastname);
+
         }
     }
 }
